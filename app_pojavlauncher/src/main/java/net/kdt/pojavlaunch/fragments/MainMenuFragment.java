ad771bc34231f5c29709629fea14aacdabfeae02
KAZU_KUNN <149859518+KAZU-YAA@users.noreply.github.com>
2025-05-23 17:10:01 +0900

    Update MainMenuFragment.java

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/fragments/MainMenuFragment.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/fragments/MainMenuFragment.java
@@ -64,15 +64,7 @@ public class MainMenuFragment extends Fragment {
 
         mShareLogsButton.setOnClickListener((v) -> shareLog(requireContext()));
 
-        mOpenDirectoryButton.setOnClickListener((v)-> {
-            Tools.switchDemo(Tools.isDemoProfile(v.getContext())); // avoid switching accounts being able to access
-            if(Tools.isDemoProfile(v.getContext())){
-                Toast.makeText(v.getContext(), R.string.toast_not_available_demo, Toast.LENGTH_LONG).show();
-                return;
-            }
-
-            openPath(v.getContext(), getCurrentProfileDirectory(), false);
-        });
+        mOpenDirectoryButton.setOnClickListener((v)-> openPath(v.getContext(), getCurrentProfileDirectory(), false));
 
 
         mNewsButton.setOnLongClickListener((v)->{
