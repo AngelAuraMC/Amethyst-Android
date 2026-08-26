e9c795ca3288f81d64a1cc7948f05b136d7834fb
tomikun <60690056+alexytomi@users.noreply.github.com>
2026-08-26 21:24:47 +0800

    IT WAS SUCH A FUCKING TINY TYPO HOLY

--- app_pojavlauncher/src/main/jni/driver_helper/internal_android_dlopen_hook/turnip/hook.c
+++ app_pojavlauncher/src/main/jni/driver_helper/internal_android_dlopen_hook/turnip/hook.c
@@ -47,10 +47,10 @@ __attribute__((visibility("default"), used)) void *android_dlopen_ext(const char
                 // libvulkan should feed itself here so we inherit all its loaded NEEDED sonames
                 // which are all of turnips NEEDED too.
                 extinfo->library_namespace,
-                privateDlFuncs->dlopen_ext
+                __builtin_return_address(0)
                 );
-        void* turnip_driver_handle = linker_ns_dlopen("libvulkan_freedreno.so", RTLD_LOCAL | RTLD_NOW, turnipNs);
-        if(turnip_driver_handle == NULL) {
+        turnipHandle = linker_ns_dlopen("libvulkan_freedreno.so", RTLD_LOCAL | RTLD_NOW, turnipNs);
+        if(turnipHandle == NULL) {
             printf("AdrenoSupp: Failed to load Turnip!\n%s\n", dlerror());
             return NULL;
         }
--- app_pojavlauncher/src/main/jni/egl_bridge.c
+++ app_pojavlauncher/src/main/jni/egl_bridge.c
@@ -160,6 +160,7 @@ int pojavInitOpenGL() {
             load_vulkan();
             setenv("GALLIUM_DRIVER", "zink", 1);
             setenv("MESA_ANDROID_NO_KMS_SWRAST", "1", 1);
+            setenv("MESA_LOADER_DRIVER_OVERRIDE", "kgsl", true);
         } else if (!strcmp(renderer, "opengles3_desktopgl_freedreno")) {
             setenv("GALLIUM_DRIVER", "freedreno", 1);
             setenv("MESA_LOADER_DRIVER_OVERRIDE", "kgsl", 1);
