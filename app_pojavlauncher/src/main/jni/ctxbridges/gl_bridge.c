ca25b0f842391323e8cc95fbd4a136f0457bc520
whitebelyash <whbexiumwork@gmail.com>
2026-08-28 18:12:30 +0400

    fix(pojavexec): improve EGL config selection logic

--- app_pojavlauncher/src/main/jni/ctxbridges/gl_bridge.c
+++ app_pojavlauncher/src/main/jni/ctxbridges/gl_bridge.c
@@ -61,9 +61,18 @@ gl_render_window_t* gl_init_context(gl_render_window_t *share) {
     const char* renderer = getenv("AMETHYST_RENDERER");
     bool gl_ctx = strncmp(renderer, "opengles3_desktopgl", 19) == 0;
     // Freedreno refuses to give EGL configs even with GL bit set if ES bit is present
-    // This sucks, but whatever, I don't wanna rebuild my Mesa
-    int attr_gl = gl_ctx ? EGL_OPENGL_BIT : EGL_OPENGL_ES2_BIT;
-    EGLint egl_attributes[] = { EGL_BLUE_SIZE, 8, EGL_GREEN_SIZE, 8, EGL_RED_SIZE, 8, EGL_ALPHA_SIZE, 0, EGL_DEPTH_SIZE, 16, EGL_SURFACE_TYPE, EGL_WINDOW_BIT | EGL_PBUFFER_BIT, EGL_RENDERABLE_TYPE, attr_gl, EGL_NONE };
+    // Additionally it requires different depth/alpha size or we'll receive zero matching configs
+    // Ugh...
+    EGLint egl_attributes[] = {
+            EGL_BLUE_SIZE, 8,
+            EGL_GREEN_SIZE, 8,
+            EGL_RED_SIZE, 8,
+            EGL_ALPHA_SIZE, gl_ctx ? 0 : 8,
+            EGL_DEPTH_SIZE, gl_ctx ? 16 : 24,
+            EGL_SURFACE_TYPE, EGL_WINDOW_BIT | EGL_PBUFFER_BIT,
+            EGL_RENDERABLE_TYPE, gl_ctx ? EGL_OPENGL_BIT : EGL_OPENGL_ES2_BIT,
+            EGL_NONE
+    };
     EGLint num_configs = 0;
 
     if (eglChooseConfig_p(g_EglDisplay, egl_attributes, NULL, 0, &num_configs) != EGL_TRUE) {
