6b7bedeb48d371cafbf0de3cbbd42bddc8b3f959
tomikun <60690056+alexytomi@users.noreply.github.com>
2026-09-02 12:31:14 +0800

    remove(Settings/MobileGlues): Multidraw emulation option
    I cannot be bothered to add in the benchmarker the plugin has nor the
    options for it. We're deprecating this soon anyway for MobileGL

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/LauncherPreferences.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/LauncherPreferences.java
@@ -253,9 +253,6 @@ public class LauncherPreferences {
         MGConfigJson.put("angleDepthClearFixMode", angleDepthClearFixMode);
         MGConfigJson.put("enableExtTimerQuery", timerQueryExt);
         MGConfigJson.put("enableExtDirectStateAccess", dsaExt);
-        if (DEFAULT_PREF.getBoolean("mg_renderer_multidrawCompute", false)) {
-            MGConfigJson.put("multidrawMode", 5); // Special handling for the (special mayhaps) compute emulation
-        } else MGConfigJson.put("multidrawMode", Integer.parseInt(DEFAULT_PREF.getString("mg_renderer_setting_multidraw", "0")));
         MGConfigJson.put("maxGlslCacheSize", Integer.parseInt(DEFAULT_PREF.getString("mg_renderer_setting_glsl_cache_size", "128")));
         File configFile = new File(Tools.DIR_DATA + "/MobileGlues", "config.json");
         FileUtils.ensureParentDirectory(configFile);
